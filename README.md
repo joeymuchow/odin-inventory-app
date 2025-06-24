018aef3487107573450d1ea477c7293d41adf497
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-06-24 12:14:33 -0700

    Create index view and create routers
    The index view has links to go to each route(games, genres, platforms,
    and developers). The routers for those 4 exist, but still need to be
    really created.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-inventory-app
+
+This project is a part of the Node.js course from The Odin Project where we are making an express app that manages inventory for a store. For my project, I am making an inventory system that tracks video games in the system and has information for games, genres, platforms, and developers.
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 import express from "express";
-
+import 'dotenv/config';
+import developerRouter from "./routes/developerRouter.js";
+import gameRouter from "./routes/gameRouter.js";
+import genreRouter from "./routes/genreRouter.js";
+import platformRouter from "./routes/platformRouter.js";
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+const __dirname = import.meta.dirname;
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/developers", developerRouter);
+app.use("/games", gameRouter);
+app.use("/genres", genreRouter);
+app.use("/platforms", platformRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
